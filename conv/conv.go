690b61e3030ab76c862d0c5b32d759b011418335
gorankvi <goran.kvitne@gmail.com>
2023-02-08 17:59:56 +0100

    Skrevet kode og test for hver funksjon

--- conv/conv.go
+++ conv/conv.go
@@ -1,33 +1,37 @@
 package conv
 
+import "math"
+
 // Konverterer Farhenheit til Celsius
+
 func FarhenheitToCelsius(value float64) float64 {
-	
-	return (value - 32)*(5/9)
-}
+		result := (value - 32) * (5.0 / 9.0)
+		return math.Round(result*100) / 100
+	}
+
 // Konverterer Farhenheit til Kelvin
 func FarhenheitToKelvin(value float64) float64{
-	
-	return (value -32) * (5/9) + 273.15
+	result:= (value - 32.0) * (5.0/9.0) + 273.15
+	return math.Round(result*100) / 100
 }
 // Konverterer Celsius til Fahrenheit
 func CelsiusToFahrenheit(value float64) float64{
-	
-	return value * (9/5) + 32
+	result := value * (9.0/5.0) + 32.0
+	return math.Round(result*100) / 100
 }
 // Konverterer Celsius til Kelvin
 func CelsiusToKelvin(value float64) float64{
-	
-	return value - 273.15
+	result := value + 273.15
+	return math.Round(result*100) / 100
 }
 // Konverterer Kelvin til Farhenheit
 func KelvinToFarhenheit(value float64) float64{
-	
-	return (value - 273.15)*(9/5)+32
+	result := (value - 273.15)*(9.0/5.0)+32.0
+	return math.Round(result*100) / 100
 }
 // Konverterer Kelvin til Celsius
 func KelvinToCelsius(value float64) float64{
-	
-	return value + 273.15
+	result := value - 273.15
+	return math.Round(result*100) / 100
 }
 
--- conv/conv_test.go
+++ conv/conv_test.go
@@ -5,15 +5,7 @@ import (
 	"testing"
 )
 
-/*
-*
-
-	Mal for testfunksjoner
-	Du skal skrive alle funksjonene basert på denne malen
-	For alle konverteringsfunksjonene (tilsammen 6)
-	kan du bruke malen som den er (du må selvsagt endre
-	funksjonsnavn og testverdier)
-*/
+// Tester at funksjonen FarhenheitToCelsius gir 56.67 ved input 134: PASS
 func TestFarhenheitToCelsius(t *testing.T) {
 	type test struct {
 		input float64
@@ -32,5 +24,93 @@ func TestFarhenheitToCelsius(t *testing.T) {
 	}
 }
 
-// De andre testfunksjonene implementeres her
-// ...
+// Tester at FarhenheitToKelvin gir 329.82 ved input 134.01: PASS
+func TestFarhenheitToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 134.01, want: 329.82},
+	}
+
+	for _, tc := range tests {
+		got := FarhenheitToKelvin(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+// Tester at CelsiusTOFahrenheit gir 134.01 ved input 56.67: PASS
+func TestCelsiusToFahrenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 56.67, want: 134.01},
+	}
+
+	for _, tc := range tests {
+		got := CelsiusToFahrenheit(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+// Tester at CelsiusToKelvin gir 329.82 ved input 56.67: PASS
+func TestCelsiusToKelvin(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 56.67, want: 329.82},
+	}
+
+	for _, tc := range tests {
+		got := CelsiusToKelvin(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+// Tester at KelvinToFarhenheit gir 134.01 ved input 329.82: PASS
+func TestKelvinToFarhenheit(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 329.82, want: 134.01},
+	}
+
+	for _, tc := range tests {
+		got := KelvinToFarhenheit(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
+// Tester at KelvinToCelsius gir 56.67 ved input 329.82: PASS
+func TestKelvinToCelsius(t *testing.T) {
+	type test struct {
+		input float64
+		want  float64
+	}
+
+	tests := []test{
+		{input: 329.82, want: 56.67},
+	}
+
+	for _, tc := range tests {
+		got := KelvinToCelsius(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
